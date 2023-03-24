ec06826e3113d766a00e2d1bdb02a40ef2474d94
Olga <olgajensen@mail.ru>
2023-03-24 21:47:25 +0300

    1

---
+++ Task_4_1.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+// 1. Пусть дан LinkedList с несколькими элементами. 
+// Реализуйте метод(не void), который вернет “перевернутый” список.
+
+public class Task_4_1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        
+        
+    }
+
+    
+}
